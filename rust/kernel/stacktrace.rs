d254e9e84b3c69dab69c37e60707dc2b928a819b
Antoine Doglioli <antoine.doglioli@gmail.com>
2025-01-15 17:04:00 +0100

    Minor refactoring changes

--- rust/kernel/stacktrace.rs
+++ rust/kernel/stacktrace.rs
@@ -3,8 +3,13 @@
 //! C header : [`arch/x86/include/stacktrace.h`](../../../../include/linux/stacktrace.h)
 
 use crate::{
-    alloc::{allocator::Kmalloc, IntoIter, KVec},
+    alloc::{allocator::Kmalloc, Flags, IntoIter, KVec},
+    impl_has_work,
+    kernel::error::Result,
+    list::List,
     prelude::GFP_KERNEL,
+    sync::{Arc, SpinLock},
+    workqueue::{Work, WorkItem},
 };
 
 /// Represent a captured stacktrace of the current process
@@ -12,14 +17,14 @@ pub struct Stacktrace(KVec<u64>);
 
 impl Stacktrace {
     /// Save a new stacktrace of the current process
-    pub fn new(size: usize) -> Self {
-        let mut buf = KVec::from_elem(0u64, size, GFP_KERNEL)?;
+    pub fn new(size: usize, flag: Flags) -> Result<Self> {
+        let mut buf = KVec::from_elem(0u64, size, flag)?;
 
         let len = unsafe { bindings::stack_trace_save(buf.as_mut_ptr(), size as _, 0) };
 
         // SAFETY : We have by the `stack_trace_save` contract that `len<size` so `new_len<old_len`.
-        unsafe { buf.set_len(len) };
-        Stacktrace(buf)
+        unsafe { buf.set_len(len as _) };
+        Ok(Stacktrace(buf))
     }
 }
 
